ae51dd61049dcd30c6da805a19d3cc1dfe8f9e73
Aasem <aasem.rahuma@gmail.com>
2023-05-30 18:26:09 -0400

    implementing eqarrays with function assertequal

---
+++ eqArrays.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion failed: " + actual + " !== " + expected);
+  }
+};
+const eqArrays = function (arr1, arr2){
+  for(let x = 0; x < arr1.length; x++) {
+    if(arr1[x] !== arr2[x])
+    return false;
+  }
+  return true;
+}
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
