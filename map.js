2995e6ad9e8fb0623997ed4da206677ee13f77b2
Aasem <aasem.rahuma@gmail.com>
2023-06-02 08:24:23 -0400

    implementing array using .map

---
+++ map.js
@@ -0,0 +1,26 @@
+const assertArrayEqual = function(actual, expected) {
+  let equivalentArrays = eqArrays(actual, expected);
+  if (equivalentArrays === true) {
+    console.log(` Assertion Passed: ${actual} === ${expected}`);
+  } else if (equivalentArrays === false) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2){
+  for(let x = 0; x < arr1.length; x++) {
+    if(arr1[x] !== arr2[x])
+    return false;
+  }
+  return true;
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
