314bc4c48fc17af6f18308c63e3dfd62dd40ea4c
Aasem Rahuma <aasem.rahuma@gmail.com>
2023-06-05 17:54:12 -0400

    refactoring to use mocha chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,14 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
 const assertEqual = require('../assertEqual');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); 
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  const words = ["Yo Yo", "Lighthouse", "Labs"];
+  it("returns 3 for words.length", () => {
+    assert.strictEqual((words.length), 3);
+  });
+});
+
+//const words = ["Yo Yo", "Lighthouse", "Labs"];
+//tail(words); 
+//assertEqual(words.length, 3);
